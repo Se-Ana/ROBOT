4409a7f0378b698db000a53bf39e79bb04c36007
Se-Ana <YaSeana@ya.ru>
2023-12-03 14:28:57 +0300

    Начинаем работу над роботом, собирающим дев через UI

---
+++ src/test/java/CLM/utils/StoreBuilder.java
@@ -0,0 +1,33 @@
+package CLM.utils;
+
+import org.junit.jupiter.api.Test;
+
+public class StoreBuilder {
+    public static void main(String[] args) {
+        String loginS = " ";
+        String passS = "";
+    }
+
+    public String buildDEV() {
+        System.out.println("Helo");
+        //перейти на сайт http://tkles-efs000545.vm.esrt.cloud.sbrf.ru:8443/ui/index.html#/home
+        //залогиниться под своей учёткой Sigma
+        //убедиться, что мы на странице сборки нужного пайма (проверить название)
+        //нажать "Собрать с параметрами"
+        //получить имя билда
+        //нажать "Собрать"
+        //убедиться, что сборка запущена
+        //подождать пока билд соберётся
+        //проверить статус сборки при её завершении
+        //сообщить о статусе текстовым сообщением на экране
+
+        //при успехе вернуть имя билда
+        //иначе вернуть сообщение об ошибке в лог
+        String devBuildName = "ж";
+        System.out.println("Hello");
+
+        return devBuildName;
+    }
+}
+
+
