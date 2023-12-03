b2631eb401f3358cd7db0ff6de73076f152e5346
Se-Ana <YaSeana@ya.ru>
2023-12-03 14:14:25 +0300

    Сформирована структура проекта

---
+++ src/test/java/CLM/utils/Builder.java
@@ -0,0 +1,15 @@
+package CLM.utils;
+
+import org.junit.jupiter.api.Test;
+
+public class Builder {
+
+    @Test
+    void buildDEV() {
+        System.out.println("Helo");
+        //перейти на сайт http://tkles-efs000545.vm.esrt.cloud.sbrf.ru:8443/ui/index.html#/home
+        //залогиниться под своей учёткой Sigma
+
+
+    }
+}
